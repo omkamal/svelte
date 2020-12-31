1cfd748f8ad2efd6245f7d5bf92d33991e41d4b6
omar hosney <omkamal@gmail.com>
2020-12-30 17:02:03 -0800

    Loan Calculation

--- loan/src/App.svelte
+++ loan/src/App.svelte
@@ -1,10 +1,41 @@
 <script>
 	export let name;
+	let interestRate = 3;
+	let loanAmount = 200000;
+	let years = 30;
+	const MONTHS_PER_YEAR = 12;
+
+	$: months = years * MONTHS_PER_YEAR;
+	$: monthlyInterestRate = interestRate / 100 / MONTHS_PER_YEAR;
+	$: numerator = loanAmount * monthlyInterestRate;
+	$: denominator = 1 - (1 + monthlyInterestRate) ** -months;
+	$: payment = !loanAmount || !years ? 0: 
+	interestRate ? numerator / denominator :
+	loanAmount / months;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<label for="loan">Loan Amount</label>
+	<input id="loan" type="number" bind:value={loanAmount}>
+
+	<label for="interest">Interest Rate</label>
+	<input id="interest" type="number" bind:value={interestRate}>
+
+	<label for="years">Years</label>
+	<input id="years" type="number" bind:value={years}>
+
+	<div>
+		monthlyInterestRate: {monthlyInterestRate} 
+		<br>
+		numerator: {numerator}
+
+		<p>
+			Montly Payment: ${payment.toFixed(2)}
+		</p>
+		
+	</div>
+
 </main>
 
 <style>
